3af54fd126409bf4b318336d2e2630e5e394c5d5
Naveed Aheer <naveedaheerpc@gmail.com>
2022-01-27 22:22:53 +0500

    Removed extra files

--- server/routes/fetchRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { fetchController } = require("../Controller/fetchController");
-const router = express.Router();
-
-router.route("/").get(fetchController)
-
-
-module.exports = router
