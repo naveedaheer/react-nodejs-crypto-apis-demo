730c427d679e44f69a291804400a0d8b3d5774e3
Naveed Aheer <naveedaheerpc@gmail.com>
2022-01-28 13:24:41 +0500

    Updated interface for order books

--- client/src/types/common.types.ts
+++ client/src/types/common.types.ts
@@ -1,11 +1,8 @@
-export interface Common {
-  symbol: string;
-  bidPrice: string;
-  bidQty: string;
-  askPrice: string;
-  askQty: string;
+export interface OrderBook {
+  lastUpdateId: number;
+  bids: [string[]];
+  asks: [string[]];
 }
-
 export interface CurrencyPair {
   symbol: string;
   price: string;
