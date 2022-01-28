645e1fe361ba1c3bb1802c33c26e589b18e51b24
Naveed Aheer <naveedaheerpc@gmail.com>
2022-01-28 15:48:12 +0500

    quick fix

--- server/index.js
+++ server/index.js
@@ -2,35 +2,10 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const orderBookController = require("./src/order-book/controller");
+
 app.use(express.json());
 app.use(cors());
-const server = require("http").createServer(app);
-const WebSocket = require("ws");
-const wss = new WebSocket.Server({ server: server });
-
-wss.on("connection", function connection(ws) {
-  console.log("A new client Connected!");
-  ws.send("Welcome New Client!");
-  setInterval(() => {
-    const msg = {
-      name: "test",
-      message: "message",
-    };
-    ws.send(JSON.stringify(msg));
-  }, 1000);
-  ws.on("message", function incoming(message) {
-    console.log("received123444", message);
-
-    wss.clients.forEach(function each(client) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message);
-      }
-    });
-  });
-});
-
 app.use("/order-book", orderBookController);
-app.use(express.json());
-app.use(cors());
 
-server.listen(8000, () => console.log(`Lisening on port :8000`));
+const PORT = 8000;
+app.listen(PORT, () => console.log(`Lisening on port ${PORT}`));
