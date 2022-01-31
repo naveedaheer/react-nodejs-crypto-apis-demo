b0705e41ce733fcfe0d74a5ffa936bd45bbab743
Naveed Aheer <naveedaheerpc@gmail.com>
2022-01-31 20:54:53 +0500

    change server file

--- server/server.js
+++ server/server.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 const app = express();
 const cors = require("cors");
 const orderBookController = require("./src/order-book/controller");
+app.use(cors())
+app.options('*', cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    parameterLimit: 100000,
+    limit: '50mb',
+    extended: true
+}));
 
-app.use(express.json());
-app.use(cors());
 app.use("/order-book", orderBookController);
 
 app.get('/', (req, res) => res.send('Server Up and Running'))
