ca49cf6b95c4526cbb41de3bd10b0c6ec7388137
Naveed Aheer <naveedaheerpc@gmail.com>
2022-02-01 12:33:42 +0500

    refactoring for web sockets

--- server/server.js
+++ server/server.js
@@ -1,39 +1,13 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const orderBookController = require("./src/order-book/controller");
-const server = require("http").createServer(app);
-const WebSocket = require("ws");
-const wss = new WebSocket.Server({ server });
-
 app.use(cors())
 app.options('*', cors());
 
-wss.on("connection", function connection(ws) {
-  console.log("A new client Connected!");
-  ws.send("Welcome New Client!");
-  setInterval(() => {
-    // const msg = {
-    //   name: "test",
-    //   message: "message",
-    // };
-    // ws.send(JSON.stringify(msg));
-  }, 1000);
-  ws.on("message", function incoming(message) {
-    console.log("received1234445", message);
-
-    wss.clients.forEach(function each(client) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message);
-      }
-    });
-  });
-});
-
+const orderBookController = require("./src/order-book/controller");
 app.use("/order-book", orderBookController);
 
 app.get('/', (req, res) => res.send('Server Up and Running'))
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Lisening on port ${PORT}`));
-module.exports = wss;
